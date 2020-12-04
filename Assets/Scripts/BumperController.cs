383cdcac2d7b676389063ba855df7214f7403926
kazuki okazaki <prunzfrfx@gmail.com>
2020-12-04 14:12:49 +0900

    first commit

---
+++ Assets/Scripts/BumperController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    //private float power = 200.0f;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision opponent)
+    {
+        if ( "Ball" == opponent.gameObject.name ) {
+            _animator.Play("Bumper1Animation");
+            //_animator.SetTrigger("BallHitTrigger");
+            Debug.Log("あたった");
+            //Rigidbody playerRigid = opponent.gameObject.GetComponent<Rigidbody>();
+            //playerRigid.AddForce(-playerRigid.velocity * power);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameMainController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMainController : MonoBehaviour
+{
+    public GameObject _objBall;
+    public GameObject _objShooter;
+    public GameObject _objFlipperL;
+    public GameObject _objFlipperR;
+    private HingeJoint _hingeJointL; // AxisL
+    private HingeJoint _hingeJointR; // AxisR
+    private JointSpring _jointSpringL; // AxisL
+    private JointSpring _jointSpringR; // AxisR
+    public float spring = 40000;
+    public float openAngle = 30; // 開く角度
+    public float closeAngle = -30; // 閉じる角度
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start() - GameMainContloller");
+        _hingeJointL = _objFlipperL.GetComponent<HingeJoint>();
+        _jointSpringL = _hingeJointL.spring;
+        _hingeJointR = _objFlipperR.GetComponent<HingeJoint>();
+        _jointSpringR = _hingeJointR.spring;
+        // ボールを初期位置に移動
+        InitBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左キーを押す
+         if ( Input.GetKey (KeyCode.LeftArrow) ) {
+            _jointSpringL.spring = spring;
+            _jointSpringL.targetPosition = closeAngle;
+            _hingeJointL.spring = _jointSpringL;
+        } else {
+            _jointSpringL.spring = spring;
+            _jointSpringL.targetPosition = openAngle;
+            _hingeJointL.spring = _jointSpringL;
+        }
+
+        if ( Input.GetKey (KeyCode.RightArrow) ) {
+            _jointSpringR.spring = spring;
+            _jointSpringR.targetPosition = openAngle;
+            _hingeJointR.spring = _jointSpringR;
+        } else {
+            _jointSpringR.spring = spring;
+            _jointSpringR.targetPosition = closeAngle;
+            _hingeJointR.spring = _jointSpringR;
+        }
+
+    }
+
+    // ボールを初期位置に移動
+    public void InitBall()
+    {
+        Vector3 pos = _objBall.transform.position;
+        pos.x = 3.21f;
+        pos.y = 0.26f;
+        pos.z = 0.0f;
+        _objBall.transform.position = pos;
+    }
+
+    // ボールを発射　※あとで発射台に押してもらう方式に切り替える
+    public void ShotBall()
+    {
+        Debug.Log("タッチされたにゃー");
+        Rigidbody rb = _objBall.GetComponent<Rigidbody> ();
+        Vector3 force = new Vector3 (0.0f, 700.0f, 700.0f);
+        // ForceMode Force Acceleration Impulse VelocityChange
+        rb.AddForce (force, ForceMode.Acceleration);
+    }
+}
